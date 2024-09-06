2e12690eb8dcc4d4cd56a956b653b2a63781e471
DanielMen97 <133619338+DanielMen97@users.noreply.github.com>
2024-09-05 23:35:15 -0500

    login

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,14 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons/faUserAstronaut'
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import {
+  faUserAstronaut,
+  faLock,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
     <View style={styles.loginContainer}>
-      <FontAwesomeIcon icon={faUserAstronaut} style={{color: 'transparent'}}/>
-      <View style={{flexDirection: 'row'}}>
+      <FontAwesomeIcon size={100} icon={faUserAstronaut} style={styles.logo} />
+      <View style={styles.title}>
         <Text style={styles.nameApp}>SGRTMobile</Text>
         <Text style={styles.labelApp}>App</Text>
+      </View>
+      <View style={styles.formLogin}>
+        <View style={styles.formInput}>
+          <FontAwesomeIcon size={25} style={styles.formIcon} icon={faUser} />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder="Correo electronico"
+            placeholderTextColor="#FFFFFF"
+            keyboardType="email-address"
+          />
+        </View>
+        <View style={styles.formInput}>
+          <FontAwesomeIcon size={25} style={styles.formIcon} icon={faLock} />
+          <TextInput
+            style={styles.formTextInput}
+            placeholder="Contraseña"
+            placeholderTextColor="#FFFFFF"
+            keyboardType="default"
+            secureTextEntry={true}
+          />
+        </View>
+        <Pressable style={styles.buttonLogin}>
+          <Text style={styles.labelButton}>Ingresar</Text>
+        </Pressable>
+        <Text style={styles.version}>Version 1.001</Text>
+        <Text style={styles.colaborators}>By Andres Ruiz Deibyth Padilla Cristian Mendoza</Text>
       </View>
     </View>
   );
@@ -16,19 +46,78 @@ const Home = () => {
 
 const styles = StyleSheet.create({
   loginContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 300,
-    height: 100,
-    borderColor: 'red',
-    borderWidth: 2
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    color: "#ffffff",
+    marginBottom: 20,
+  },
+  title: {
+    flexDirection: "row",
+    marginBottom: 40,
   },
   nameApp: {
-    color: "#ffffff"
+    color: "#ffffff",
+    fontSize: 30,
   },
   labelApp: {
+    fontWeight: "900",
+    fontSize: 30,
     color: "#ffffff",
-    fontWeight: '900'
+  },
+  formLogin: {},
+  formInput: {
+    flexDirection: "row",
+    alignItems: "center",
+    textAlign: "center",
+    borderColor: "#ffffff",
+    borderWidth: 2,
+    width: 300,
+    height: 55,
+    marginHorizontal: 8,
+    marginBottom: 25,
+    padding: 3,
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  formIcon: {
+    position: "absolute",
+    left: 8,
+    color: "#ffffff",
+    backgroundColor: "transparent",
+  },
+  formTextInput: {
+    width: "100%",
+    alignSelf: "center",
+    textAlign: "center",
+    color: "#ffffff",
+    fontSize: 18,
+  },
+  buttonLogin: {
+    width: 300,
+    height: 55,
+    borderRadius: 8,
+    paddingVertical: 9,
+    marginTop: 30,
+    marginHorizontal: 8,
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 70
+  },
+  labelButton: {
+    fontSize: 20,
+  },
+  version:{
+    textAlign: 'center',
+    color: "#ffffff",
+    fontStyle: 'italic'
+  },
+  colaborators:{
+    textAlign: 'center',
+    color: "#ffffff",
+    fontStyle: 'italic'
   }
 });
 
